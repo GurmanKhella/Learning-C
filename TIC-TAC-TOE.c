7d2d1e40a2c02a7df5fc9e898b7be99c52e24f89
GurmanKhella <gurmankhella@gmail.com>
2024-11-07 17:37:52 -0800

    TIC-TAC-TOE revised version
    I changed variables, created new functions to make the code more readable and organized. There were a few errors with each of the functions, and their logic, and  I still need to write a function to check who won, and overall improve my code, so that it is a more complete version of the game.

--- TIC-TAC-TOE.c
+++ TIC-TAC-TOE.c
@@ -1,53 +1,76 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#define SIZE 9
 
-char positions[9] = {'1','2','3','4','5','6','7','8','9'};
+char positions[SIZE] = {'1','2','3','4','5','6','7','8','9'};
 char symbol;
+char playerInput = '0';
 
-void makeMove(char inputPosition, char symbol);
+void intro();
+void makeMove(char inputPosition);
+void printBoard();
+void playerUI(char inputPosition);
+bool checkInput(char inputPosition);
 
 int main(){
-    
-    
+    intro();
+    for(int i = 0; i < SIZE; i++) {
+        playerUI(playerInput);
+    }
 }
 
-void printboard(char positions[], char symbol){
-
-
-
-}
-void Intro(){
-
-    int playerOne = 0;
-    
+void intro() {
     printf("This is the Grid format: \n");
-    
     printf("1|2|3\n-----\n4|5|6\n------\n7|8|9\n");
-
     printf("Note: Choose your position by enter in the corresponding number on the grid\n ");
-
+    
     do {
-        printf("\nChoose your symbol(x or o):");
+        printf("\nChoose your symbol(x or o): ");
         scanf(" %c", &symbol);
     } while(symbol != 'x' && symbol != 'o');
-    
+}
+
+void playerUI(char inputPosition) {
     if (symbol == 'x'){
-        printf("You get to go first!\nPlay your first turn: ");
-        scanf("%c", &playerOne);
-        
-        if(playerOne != positions){
-            do{
-                printf("\nPlease enter in a valid position: ");
-                scanf(" %d", &playerOne);
-            } while(playerOne != positions);
-        }
+        printf("Your turn Player x: ");  
+        scanf(" %c", &playerInput);
+        checkInput(playerInput);
+        makeMove(playerInput);
+        printBoard();
+        symbol = 'o';
     }
     else{
-        printf("\nYou are going second... loser");
+        printf("\nYour turn player o: ");
+        scanf(" %c", &playerInput);
+        checkInput(playerInput);
+        makeMove(playerInput);
+        printBoard();
+        symbol = 'x';
     }
 }
-void makeMove(char inputPosition, char symbol) {
-    if(inputPosition == positions[(int)inputPosition -1]){
-        positions[(int)inputPosition] = symbol;
+
+void makeMove(char inputPosition) {
+    if(inputPosition == positions[(int)inputPosition - 49]){
+        positions[(int)inputPosition - 49]= symbol;
     }
+}
+
+void printBoard() { 
+    int index = 0;
+
+    for(int y = 0; y < SIZE/3; y++){
+        for(int x = 0; x < SIZE/3; x++){
+            printf("%3c", positions[index]);
+            index++;
+        }
+        printf("\n");
+    }    
+}
+
+bool checkInput(char inputPosition) {
+    if (positions[(int)inputPosition - 1] == inputPosition) {
+        return true;
+    }
+    return false;
 }
